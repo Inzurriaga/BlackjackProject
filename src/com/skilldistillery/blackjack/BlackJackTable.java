177f04424d37accd767195478458943c053430e2
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-12-06 15:57:21 -0700

    players in play can hit or stay

--- src/com/skilldistillery/blackjack/BlackJackTable.java
+++ src/com/skilldistillery/blackjack/BlackJackTable.java
@@ -10,73 +10,106 @@ public class BlackJackTable {
 	private List<Character> playersInGame;
 	private Deck deck;
 	private Scanner input;
-	
+
 	public BlackJackTable() {
 		this.characters = new ArrayList<>();
 		this.playersInGame = new ArrayList<>();
 		this.deck = new Deck();
 		this.input = new Scanner(System.in);
 	}
-	
+
 	public void startGame() {
 		deck.shuffle();
 		addDealer();
 		placeBet();
 		dealCards();
+		playerPlay();
 	}
-	
+
 	public void addPlayer(Character player) {
 		this.characters.add(player);
 	}
-	
+
 	public void addDealer() {
 		this.characters.add(new Dealer("Dealer"));
 	}
-	
+
 	public void placeBet() {
 		playersInGame.clear();
-		for(Character character : characters) {
-			if(character instanceof Dealer) {
-				System.out.println("hello im dealer");
+		for (Character character : characters) {
+			if (character instanceof Dealer) {
 				playersInGame.add(character);
 				break;
 			}
 			int playersChoice;
-			while(true) {
+			while (true) {
 				System.out.println(character.getName() + " pick an option");
 				playerBetOption();
 				try {
 					playersChoice = input.nextInt();
+					if (playersChoice == 3) {
+						System.out.println("You have " + ((Player) character).getWallet() + " in your wallet");
+						continue;
+					}
 					break;
-				} catch(InputMismatchException e) {
+				} catch (InputMismatchException e) {
 					System.err.println("enter a valid response");
 				}
 			}
-			if(playersChoice == 1) {
+			if (playersChoice == 1) {
 				playersInGame.add(character);
 				updatePlayersBet(character);
 			}
-			
+
 		}
 	}
-	
+
 	public void dealCards() {
 		System.out.println(this.playersInGame.get(this.playersInGame.size() - 1) + " is dealing out cards");
-		for(int i = 0; i < 2; i++) {
-			for(Character player : playersInGame) {
+		for (int i = 0; i < 2; i++) {
+			for (Character player : playersInGame) {
 				player.addCardToHand(this.deck.dealCard());
 			}
 		}
-		System.out.println(this.playersInGame.get(0).getHand());
-		System.out.println(this.playersInGame.get(1).getHand());
-		System.out.println(this.playersInGame);
-		System.out.println(this.playersInGame.get(this.playersInGame.size() - 1).getHand());
 	}
-	
+
+	public void playerPlay() {
+		for (Character player : playersInGame) {
+			if (player instanceof Player) {
+				System.out.println(player.getName() + " pick an option");
+				int playerChoice = 1;
+				do {
+					playerPlayOption();
+					try {
+						playerChoice = input.nextInt();
+						switch (playerChoice) {
+							case 1:
+								player.addCardToHand(deck.dealCard());
+								break;
+							case 2:
+								System.out.print("next player turn");
+								break;
+							default:
+								System.out.println("Enter a valid response");
+						}
+					} catch (InputMismatchException e) {
+						System.err.println("Enter a valid response");
+					}
+				} while (playerChoice == 1);
+			} else {
+				System.out.println("im the dealer");
+			}
+		}
+	}
+
 	public void playerBetOption() {
-		System.out.println("1. Bet\n2. Play next Deal");
+		System.out.println("1. Bet\n2. Play next Deal\n3. check Wallet");
 	}
-	
+
+	public void playerPlayOption() {
+		System.out.println("1. Hit\n2. Stay");
+	}
+
 	public void updatePlayersBet(Character character) {
 		int playersbet;
 		boolean compareBetToWallet = false;
@@ -86,19 +119,10 @@ public class BlackJackTable {
 				playersbet = input.nextInt();
 				compareBetToWallet = ((Player) character).compareBetToWallet(playersbet);
 				break;
-			} catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.err.println("enter a valid response");
 			}
-		} while(compareBetToWallet);
+		} while (compareBetToWallet);
 	}
-	
-	public void playerCardOption() {
-		
-	}
-	
-	
-	
-	public void gameOption() {
-		System.out.println();
-	}
+
 }
